ab1cb4c6aa0a1372dcbddbcfb7c16d5a723c74dc
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-06-16 19:40:31 +0900

    デフォルト引数を追加

--- trinity-view/src/main/scala/org/sisioh/trinity/view/ScalateView.scala
+++ trinity-view/src/main/scala/org/sisioh/trinity/view/ScalateView.scala
@@ -3,7 +3,7 @@ package org.sisioh.trinity.view
 import org.sisioh.trinity.Config
 import org.fusesource.scalate.TemplateEngine
 
-case class ScalateView(config: Config, path: String, context: Map[String, Any]) extends View {
+case class ScalateView(config: Config, path: String, context: Map[String, Any] = Map.empty) extends View {
 
   private val engine = new TemplateEngine()
   engine.workingDirectory = config.templateWorkDir
