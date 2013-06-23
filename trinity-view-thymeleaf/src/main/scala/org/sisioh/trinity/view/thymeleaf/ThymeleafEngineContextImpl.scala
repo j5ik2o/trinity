6102314a35862790b5c7e41f909352642a84b895
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-06-23 19:12:57 +0900

    トレイトから実装を分離

--- trinity-view-thymeleaf/src/main/scala/org/sisioh/trinity/view/thymeleaf/ThymeleafEngineContextImpl.scala
+++ trinity-view-thymeleaf/src/main/scala/org/sisioh/trinity/view/thymeleaf/ThymeleafEngineContextImpl.scala
@@ -1,7 +1,27 @@
 package org.sisioh.trinity.view.thymeleaf
 
-import org.sisioh.trinity.domain.config.Config
+import org.sisioh.trinity.domain.config.{Environment, Config}
+import org.thymeleaf.TemplateEngine
+import org.thymeleaf.templateresolver.{FileTemplateResolver, ClassLoaderTemplateResolver}
 
 case class ThymeleafEngineContextImpl(implicit val config: Config)
-  extends ThymeleafEngineContext
+  extends ThymeleafEngineContext {
+
+  val engine = new TemplateEngine()
+
+  protected val resolver = if (config.environment == Environment.Product) {
+    val classLoaderTemplateResolver = new ClassLoaderTemplateResolver
+    classLoaderTemplateResolver.setPrefix(config.templatePath.drop(1))
+    classLoaderTemplateResolver
+  } else {
+    val fileTemplateResolver = new FileTemplateResolver
+    fileTemplateResolver.setPrefix(config.localDocumentRoot + config.templatePath)
+    fileTemplateResolver
+  }
+  resolver.setTemplateMode("XHTML")
+  resolver.setSuffix(".html")
+  resolver.setCacheable(true)
+  engine.setTemplateResolver(resolver)
+
+}
 
