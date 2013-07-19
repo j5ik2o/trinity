34a75636f8494fb7e35f5ee4745e0172fc66c5bd
じゅんいち☆kかとう <j5ik2o@gmail.com>
2013-07-19 20:25:34 +0900

    scaladoc

--- trinity-core/src/main/scala/org/sisioh/trinity/domain/routing/RouteRepositoryOnMemory.scala
+++ trinity-core/src/main/scala/org/sisioh/trinity/domain/routing/RouteRepositoryOnMemory.scala
@@ -2,8 +2,12 @@ package org.sisioh.trinity.domain.routing
 
 import org.sisioh.dddbase.core.lifecycle.memory.mutable.sync.SyncRepositoryOnMemorySupport
 
+/**
+ * [[org.sisioh.trinity.domain.routing.Route]]のためのリポジトリ実装。
+ */
 class RouteRepositoryOnMemory
-  extends SyncRepositoryOnMemorySupport[RouteId, Route] with RouteRepository {
+  extends SyncRepositoryOnMemorySupport[RouteId, Route]
+  with RouteRepository {
 
   type This = RouteRepositoryOnMemory
 
