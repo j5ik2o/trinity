942426e0b8e0182464df6ea1453e9d5dd97cfe23
じゅんいち☆kかとう <j5ik2o@gmail.com>
2013-07-19 20:25:34 +0900

    scaladoc

--- trinity-core/src/main/scala/org/sisioh/trinity/domain/controller/ControllerRepositoryOnMemory.scala
+++ trinity-core/src/main/scala/org/sisioh/trinity/domain/controller/ControllerRepositoryOnMemory.scala
@@ -4,6 +4,9 @@ import java.util.UUID
 import org.sisioh.dddbase.core.lifecycle.memory.mutable.sync.SyncRepositoryOnMemorySupport
 import org.sisioh.dddbase.core.model.Identity
 
+/**
+ * [[org.sisioh.trinity.domain.controller.Controller]]のためのリポジトリ実装。
+ */
 class ControllerRepositoryOnMemory
   extends SyncRepositoryOnMemorySupport[Identity[UUID], Controller]
   with ControllerRepository {
