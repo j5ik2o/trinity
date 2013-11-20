8147d5a64d7a5790dc15a1fee884109f9a1d183a
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-11-20 16:12:32 +0900

    refactor

--- trinity-example/src/main/scala/org/sisioh/trinity/example/PlayLikeApplication.scala
+++ trinity-example/src/main/scala/org/sisioh/trinity/example/PlayLikeApplication.scala
@@ -2,14 +2,14 @@ package org.sisioh.trinity.example
 
 import org.sisioh.trinity.domain.io.http.Method._
 import org.sisioh.trinity.domain.mvc.action.SimpleAction
-import org.sisioh.trinity.domain.mvc.controller.Controller
+//import org.sisioh.trinity.domain.mvc.controller.Controller
 import org.sisioh.trinity.domain.mvc.http.Response
 import org.sisioh.trinity.domain.mvc.routing.RouteDsl._
 import org.sisioh.trinity.domain.mvc.routing.RoutingFilter
 import org.sisioh.trinity.domain.mvc.{Environment, Bootstrap}
 import scala.concurrent.Future
 
-object PlayLikeApplication extends App with Controller with Bootstrap {
+object PlayLikeApplication extends App with Bootstrap {
 
   protected val environment = Environment.Development
 
