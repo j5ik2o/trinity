fb8c5d141c84a3093de44ad5bde6a3ddfe39d950
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-11-20 15:18:42 +0900

    added support for FileResourceFilter

--- trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/resource/FileResourceReadFilter.scala
+++ trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/resource/FileResourceReadFilter.scala
@@ -1,10 +1,46 @@
 package org.sisioh.trinity.domain.mvc.resource
 
-import org.sisioh.trinity.domain.mvc.SimpleFilter
-import org.sisioh.trinity.domain.mvc.http.{Request, Response}
+import java.io.{IOException, File}
+import org.apache.commons.io.IOUtils
+import org.sisioh.trinity.domain.io.buffer.ChannelBuffers
+import org.sisioh.trinity.domain.io.http.{MimeTypes, HeaderNames, ResponseStatus}
 import org.sisioh.trinity.domain.mvc.action.Action
+import org.sisioh.trinity.domain.mvc.http.{Request, Response}
+import org.sisioh.trinity.domain.mvc.{Environment, SimpleFilter}
 import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
 
-class FileResourceReadFilter extends SimpleFilter[Request, Response] {
-  def apply(requestIn: Request, action: Action[Request, Response]): Future[Response] = ???
+class FileResourceReadFilter(environment: Environment.Value, localPath: File) extends SimpleFilter[Request, Response] {
+
+  private val fileResolver = FileResourceResolver(environment, localPath)
+
+  def isValidPath(path: String): Boolean = {
+    try {
+      val fi = getClass.getResourceAsStream(path)
+      if (fi != null && fi.available > 0) true else false
+    } catch {
+      case e: Exception =>
+        false
+    }
+  }
+
+
+  def apply(requestIn: Request, action: Action[Request, Response]): Future[Response] = {
+    if (fileResolver.hasFile(requestIn.uri) && requestIn.uri != '/') {
+      (for {
+        fh <- fileResolver.getInputStream(requestIn.uri)
+        bytes <- Try(IOUtils.toByteArray(fh))
+        result <- Try(fh.read(bytes))
+        _ <- if (result > 0) Success(bytes) else Failure(new IOException())
+      } yield {
+        val response = requestIn.response
+        val mimeType = MimeTypes.fileExtensionOf('.' + requestIn.uri.toString.split('.').last)
+        val newResponse = response.withHeader(HeaderNames.ContentType, mimeType).withResponseStatus(ResponseStatus.Ok).
+          withContent(ChannelBuffers.copiedBuffer(bytes))
+        Future.successful(Response(newResponse))
+      }).get
+    } else {
+      action(requestIn)
+    }
+  }
 }
--- trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/resource/FileResourceResolver.scala
+++ trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/resource/FileResourceResolver.scala
@@ -1,5 +1,57 @@
 package org.sisioh.trinity.domain.mvc.resource
 
-class FileResourceResolver {
+import java.io.{FileNotFoundException, FileInputStream, File, InputStream}
+import org.sisioh.trinity.domain.mvc.Environment
+import scala.util.{Failure, Success, Try}
+
+case class FileResourceResolver(environment: Environment.Value, localPath: File) {
+
+  def hasFile(path: String): Boolean = {
+    if (environment == Environment.Product) {
+      hasResourceFile(path)
+    } else {
+      hasLocalFile(path)
+    }
+  }
+
+  def getInputStream(path: String): Try[InputStream] = {
+    if (environment == Environment.Product) {
+      getResourceInputStream(path)
+    } else {
+      getLocalInputStream(path)
+    }
+  }
+
+  private def getResourceInputStream(path: String): Try[InputStream] = {
+    Option(getClass.getResourceAsStream(path)).map(s => Success(s)).getOrElse(Failure(new FileNotFoundException(path)))
+  }
+
+  private def getLocalInputStream(path: String): Try[InputStream] = Try {
+    val file = new File(localPath, path)
+    new FileInputStream(file)
+  }
+
+  private def hasResourceFile(path: String): Boolean = {
+    val fi = getClass.getResourceAsStream(path)
+    try {
+      if (fi != null && fi.available > 0) {
+        true
+      } else {
+        false
+      }
+    } catch {
+      case e: Exception =>
+        false
+    }
+  }
+
+  private def hasLocalFile(path: String): Boolean = {
+    val file = new File(localPath, path)
+    if (file.toString.contains("trinity-core/src/test")) false
+    else if (!file.exists || file.isDirectory) false
+    else if (!file.canRead) false
+    else true
+  }
+
 
 }
