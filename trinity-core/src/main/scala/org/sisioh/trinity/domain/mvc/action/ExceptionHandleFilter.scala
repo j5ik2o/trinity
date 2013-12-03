5082d5f91cdb36277514d95a3955b5631e1c5505
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-12-03 15:51:30 +0900

    added Copyright text.

--- trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/action/ExceptionHandleFilter.scala
+++ trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/action/ExceptionHandleFilter.scala
@@ -1,16 +1,31 @@
+/*
+ * Copyright 2013 Sisioh Project and others. (http://sisioh.org/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.sisioh.trinity.domain.mvc.action
 
-import org.sisioh.trinity.domain.mvc.http.{Response, Request}
-import org.sisioh.trinity.domain.mvc.{GlobalSettings}
-import scala.concurrent.{ExecutionContext, Future}
 import org.sisioh.scala.toolbox.LoggingEx
+import org.sisioh.trinity.domain.mvc.GlobalSettings
 import org.sisioh.trinity.domain.mvc.filter.SimpleFilter
+import org.sisioh.trinity.domain.mvc.http.{Response, Request}
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
  * 例外をハンドリングするためのフィルター。
  *
- * @param globalSettings
- * @param executor
+ * @param globalSettings [[org.sisioh.trinity.domain.mvc.GlobalSettings]]
+ * @param executor [[scala.concurrent.ExecutionContext]]
  */
 case class ExceptionHandleFilter
 (globalSettings: Option[GlobalSettings[Request, Response]] = None)
