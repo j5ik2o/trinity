c206f5c235d105328fb791bf171e93667c47f0e3
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-12-03 12:19:13 +0900

    added logging.

--- trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/application/Application.scala
+++ trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/application/Application.scala
@@ -1,21 +1,28 @@
 package org.sisioh.trinity.domain.mvc.application
 
+import org.sisioh.config.Configuration
+import org.sisioh.trinity.domain.mvc.http.{Response, Request}
+import org.sisioh.trinity.domain.mvc.server.ServerConfigLoader
+import org.sisioh.trinity.domain.mvc.{GlobalSettings, Environment}
 import scala.concurrent.Future
 import scala.concurrent.duration.Duration
-import org.sisioh.trinity.domain.mvc.{GlobalSettings, Environment}
-import org.sisioh.config.Configuration
-import org.sisioh.trinity.domain.mvc.server.ServerConfigLoader
-import org.sisioh.trinity.domain.mvc.http.{Response, Request}
+import org.sisioh.scala.toolbox.LoggingEx
 
-trait Application {
+trait Application extends LoggingEx {
 
   protected lazy val applicationId: String = "."
 
   protected val environment: Environment.Value
 
-  protected lazy val configuration: Configuration = ServerConfigLoader.loadConfiguration(applicationId, environment).get
+  protected lazy val configuration: Configuration = withDebugScope("configuration"){
+    scopedDebug(s"applicationId = $applicationId, environment = $environment")
+    ServerConfigLoader.loadConfiguration(applicationId, environment).get
+  }
 
-  protected lazy val serverConfig = ServerConfigLoader.loadServerConfig(configuration)
+  protected lazy val serverConfig = withDebugScope("serverConfig"){
+    scopedDebug(s"applicationId = $applicationId, configuration = $configuration")
+    ServerConfigLoader.loadServerConfig(configuration)
+  }
 
   protected implicit val globalSettings: Option[GlobalSettings[Request, Response]] = None
 
