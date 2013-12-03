269a9d7697bdc6b3b4011905d99098f3d27130a5
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-12-03 16:42:30 +0900

    refactor path pattern parser

--- trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/routing/pathpattern/SinatraPathPatternParser.scala
+++ trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/routing/pathpattern/SinatraPathPatternParser.scala
@@ -7,33 +7,33 @@ case class SinatraPathPatternParser() extends RegexPathPatternParser {
 
   def apply(pattern: String): PathPattern =
     parseAll(pathPattern, pattern) match {
-      case Success(pathPattern, _) =>
-        (PartialPathPattern("^") + pathPattern + PartialPathPattern("$")).toPathPattern
+      case Success(pr, _) =>
+        (PartialPathPattern("^") + pr + PartialPathPattern("$")).toPathPattern
       case _ =>
         throw new IllegalArgumentException("Invalid path pattern: " + pattern)
     }
 
-  private def pathPattern = rep(token) ^^ {
+  private lazy val pathPattern = rep(token) ^^ {
     _.reduceLeft {
       _ + _
     }
   }
 
-  private def token = splat | namedGroup | literal
+  private lazy val token = splat | namedGroup | literal
 
-  private def splat = "*" ^^^ PartialPathPattern("(.*?)", List("splat"))
+  private lazy val splat = "*" ^^^ PartialPathPattern("(.*?)", List("splat"))
 
-  private def namedGroup = ":" ~> """\w+""".r ^^ {
+  private lazy val namedGroup = ":" ~> """\w+""".r ^^ {
     groupName => PartialPathPattern("([^/?#]+)", List(groupName))
   }
 
-  private def literal = metaChar | normalChar
+  private lazy val literal = metaChar | normalChar
 
-  private def metaChar = """[\.\+\(\)\$]""".r ^^ {
+  private lazy val metaChar = """[\.\+\(\)\$]""".r ^^ {
     c => PartialPathPattern("\\" + c)
   }
 
-  private def normalChar = ".".r ^^ {
+  private lazy val normalChar = ".".r ^^ {
     c => PartialPathPattern(c)
   }
 }
