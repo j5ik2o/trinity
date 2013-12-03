f6a937f38b43ff38d7686b485145c097cff17234
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-12-03 12:30:24 +0900

    refactor JSON4SRenderer.

--- trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/http/JSON4SRenderer.scala
+++ trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/http/JSON4SRenderer.scala
@@ -4,11 +4,26 @@ import org.json4s._
 import org.json4s.jackson.JsonMethods._
 import org.sisioh.trinity.domain.io.http.{Charset, Charsets}
 
+/**
+ * `JValue` のための[[org.sisioh.trinity.domain.mvc.http.ResponseRenderer]]。
+ *
+ * @param jValue `org.json4s.JValue`
+ * @param charset [[org.sisioh.trinity.domain.io.http.Charset]]
+ */
 case class JSON4SRenderer(jValue: JValue, charset: Charset = Charsets.UTF_8) extends ResponseRenderer {
 
   def render(responseBuilder: ResponseBuilder): Unit =
     responseBuilder.
       withContent(compact(jValue), charset).
-      withContentType("application/json; charset=" + charset.toString.toLowerCase)
+      withContentType(JSON4SRenderer.ContentTypeName + charset.toString().toLowerCase)
+
+}
+
+/**
+ * コンパニオンオブジェクト。
+ */
+object JSON4SRenderer {
+
+  private val ContentTypeName = "application/json; charset="
 
 }
