bd04d203a236b9fe8c7b73d2691e2ce31460d61d
じゅんいち☆かとう <j5ik2o@gmail.com>
2014-02-02 21:59:50 +0900

    Dependence on dddbase was removed.

--- trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/routing/RouteId.scala
+++ trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/routing/RouteId.scala
@@ -15,7 +15,6 @@
  */
 package org.sisioh.trinity.domain.mvc.routing
 
-import org.sisioh.dddbase.core.model.Identity
 import org.sisioh.trinity.domain.io.http.Methods
 import org.sisioh.trinity.domain.mvc.routing.pathpattern.PathPattern
 
@@ -25,8 +24,7 @@ import org.sisioh.trinity.domain.mvc.routing.pathpattern.PathPattern
  * @param method
  * @param pathPattern
  */
-case class RouteId(method: Methods.Value, pathPattern: PathPattern)
-  extends Identity[(Methods.Value, PathPattern)] {
+case class RouteId(method: Methods.Value, pathPattern: PathPattern) {
   def value: (Methods.Value, PathPattern) = (method, pathPattern)
 }
 
