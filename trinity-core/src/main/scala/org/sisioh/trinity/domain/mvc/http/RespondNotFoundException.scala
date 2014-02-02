3d829565ce7b7e8447b6732e0abc1fc5d38c8720
じゅんいち☆かとう <j5ik2o@gmail.com>
2014-02-02 21:59:50 +0900

    Dependence on dddbase was removed.

--- trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/http/RespondNotFoundException.scala
+++ trinity-core/src/main/scala/org/sisioh/trinity/domain/mvc/http/RespondNotFoundException.scala
@@ -15,8 +15,6 @@
  */
 package org.sisioh.trinity.domain.mvc.http
 
-import org.sisioh.dddbase.core.BaseException
-
 case class RespondNotFoundException(message: Option[String] = None, cause: Option[Throwable] = None)
-  extends BaseException(message, cause)
+  extends Exception(message.orNull, cause.orNull)
 
