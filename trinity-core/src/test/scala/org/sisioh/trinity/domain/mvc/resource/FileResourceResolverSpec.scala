9b729354599f08335ad522f79bef892b3dcc9fa0
じゅんいち☆かとう <j5ik2o@gmail.com>
2014-04-03 09:44:12 +0900

    fixed bugs.

--- trinity-core/src/test/scala/org/sisioh/trinity/domain/mvc/resource/FileResourceResolverSpec.scala
+++ trinity-core/src/test/scala/org/sisioh/trinity/domain/mvc/resource/FileResourceResolverSpec.scala
@@ -5,7 +5,12 @@ import org.sisioh.trinity.domain.mvc.Environment
 import org.specs2.mutable.Specification
 
 class FileResourceResolverSpec extends Specification {
-  val localBasePath = new File("trinity-core/src/test/resources/")
+  val current = new File(".").getAbsoluteFile
+  val localBasePath =  if (current.toString.endsWith("trinity-core/.")) {
+    new File(current, "src/test/resources/")
+  } else {
+    new File(current, "trinity-core/src/test/resources/")
+  }
   "FileResourceResolver" should {
     "be able to get input stream when development" in {
       val target = FileResourceResolver(Environment.Development, localBasePath)
