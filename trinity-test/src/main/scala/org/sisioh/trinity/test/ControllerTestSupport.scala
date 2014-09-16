efc2e60df5a7a782841dc7c6826d782feccf654b
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2014-09-17 05:00:13 +0900

    added host and port in test context

--- trinity-test/src/main/scala/org/sisioh/trinity/test/ControllerTestSupport.scala
+++ trinity-test/src/main/scala/org/sisioh/trinity/test/ControllerTestSupport.scala
@@ -1,26 +1,25 @@
 package org.sisioh.trinity.test
 
-import com.twitter.finagle.http.{Request => FinagleRequest, RequestBuilder, Method}
+import com.twitter.finagle.http.{Method, RequestBuilder, Request => FinagleRequest}
 import java.util.concurrent.TimeUnit
-import org.jboss.netty.handler.codec.http.{HttpHeaders, HttpMethod}
+import org.jboss.netty.buffer.ChannelBuffers
+import org.jboss.netty.handler.codec.http.HttpMethod
+import org.jboss.netty.util.CharsetUtil
 import org.sisioh.trinity.domain.io.http.HeaderName
 import org.sisioh.trinity.domain.mvc.GlobalSettings
 import org.sisioh.trinity.domain.mvc.http.{Request, Response}
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.Duration
 import scala.util.Try
-import org.jboss.netty.util.CharsetUtil
-import org.jboss.netty.buffer.ChannelBuffers
 
 trait ControllerTestSupport {
 
+  case class TestServer(host: Option[String] = None, port: Option[Int] = None)
+
   trait TestContext {
+    val server: TestServer
     val executor: ExecutionContext
   }
-
-  protected val serverHost: Option[String] = None
-
-  protected val serverPort: Option[Int] = None
 
   protected val globalSettings: Option[GlobalSettings[Request, Response]] = None
 
@@ -40,48 +39,46 @@ trait ControllerTestSupport {
    * @return [[com.twitter.finagle.http.Request]]
    */
   protected def newRequest
-  (method: HttpMethod,
-   path: String,
-   content: Option[Content],
-   headers: Map[HeaderName, String]): FinagleRequest = {
-      val host = serverHost.getOrElse(defaultHost)
-      val port = serverPort.getOrElse(defaultPort)
-      def url = "http://" + host + ":" + port
-      val request = content match {
-        case Some(StringContent(v)) if method == Method.Post =>
-          val httpRequest = RequestBuilder().url(url + path).
-            build(method, Some(ChannelBuffers.copiedBuffer(v, CharsetUtil.UTF_8)))
-          FinagleRequest(httpRequest)
-        case Some(MapContent(v)) if method == Method.Post =>
-          val httpRequest = RequestBuilder().url(url + path).
-            addFormElement(v.toSeq:_*).buildFormPost(false)
-          FinagleRequest(httpRequest)
-        case Some(MapContent(v)) if method == Method.Get =>
-          val params = v map {
-            case (key, value) =>
-              key + '=' + value
-          } mkString("?", "&", "")
-          val httpRequest = RequestBuilder.safeBuildGet(
-            RequestBuilder.create().url(url + path + params)
-          )
-          FinagleRequest(httpRequest)
-        case None if method == Method.Get =>
-          val httpRequest = RequestBuilder().url(url + path).buildGet()
-          FinagleRequest(httpRequest)
-        case None if method == Method.Post =>
-          val httpRequest = RequestBuilder().url(url + path).build(method, None)
-          FinagleRequest(httpRequest)
-        case Some(_) if method == Method.Get =>
-          throw new IllegalArgumentException("Illegal request argument")
-        case _ =>
-          throw new IllegalArgumentException("Illegal request argument")
-      }
-      headers.foreach {
-        header =>
-          request.httpRequest.headers.set(header._1.asString, header._2)
-      }
-      request
+  (method: HttpMethod, path: String, content: Option[Content], headers: Map[HeaderName, String])
+  (implicit testContext: TestContext): FinagleRequest = {
+    val host = testContext.server.host.getOrElse(defaultHost)
+    val port = testContext.server.port.getOrElse(defaultPort)
+    def url = "http://" + host + ":" + port
+    val request = content match {
+      case Some(StringContent(v)) if method == Method.Post =>
+        val httpRequest = RequestBuilder().url(url + path).
+          build(method, Some(ChannelBuffers.copiedBuffer(v, CharsetUtil.UTF_8)))
+        FinagleRequest(httpRequest)
+      case Some(MapContent(v)) if method == Method.Post =>
+        val httpRequest = RequestBuilder().url(url + path).
+          addFormElement(v.toSeq: _*).buildFormPost(false)
+        FinagleRequest(httpRequest)
+      case Some(MapContent(v)) if method == Method.Get =>
+        val params = v map {
+          case (key, value) =>
+            key + '=' + value
+        } mkString("?", "&", "")
+        val httpRequest = RequestBuilder.safeBuildGet(
+          RequestBuilder.create().url(url + path + params)
+        )
+        FinagleRequest(httpRequest)
+      case None if method == Method.Get =>
+        val httpRequest = RequestBuilder().url(url + path).buildGet()
+        FinagleRequest(httpRequest)
+      case None if method == Method.Post =>
+        val httpRequest = RequestBuilder().url(url + path).build(method, None)
+        FinagleRequest(httpRequest)
+      case Some(_) if method == Method.Get =>
+        throw new IllegalArgumentException("Illegal request argument")
+      case _ =>
+        throw new IllegalArgumentException("Illegal request argument")
     }
+    headers.foreach {
+      header =>
+        request.httpRequest.headers.set(header._1.asString, header._2)
+    }
+    request
+  }
 
   /**
    * テストのためのリクエストを生成する。
